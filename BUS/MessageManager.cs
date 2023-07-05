fd4b4a9588b820720b27a2135bc27bc184722c70
AnLRP <lethanhan342004@gmail.com>
2023-07-05 14:47:38 +0700

    Add project files.

---
+++ BUS/MessageManager.cs
@@ -0,0 +1,31 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SubSonic;
+
+namespace BUS
+{
+    public class MessageManager
+    {
+        public static Message InsertMessage(Message message)
+        {
+            return new MessageController().Insert(message);
+        }
+
+        public static Message GetMessageById(object id)
+        {
+            return new Select().From(Message.Schema.TableName).Where(Message.Columns.Id).IsEqualTo(id).
+                ExecuteSingle<Message>();
+        }
+
+        public static List<Message> GetListMessage()
+        {
+            return new Select().From(Message.Schema.TableName).ExecuteTypedList<Message>();
+        }
+
+
+    }
+}
---
+++ GUI/WebForm1.aspx.cs
@@ -0,0 +1,26 @@
+﻿using BUS;
+using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GUI
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Message item = new Message();
+            // item.Content n
+            MessageManager.InsertMessage(item);
+
+
+            List<Message> lstMessage = MessageManager.GetListMessage();
+
+
+        }
+    }
+}
