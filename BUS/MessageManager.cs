1181bc55daa38439be06396f3a50f930287de1e4
AnLRP <lethanhan342004@gmail.com>
2023-07-06 09:07:57 +0700

    .

--- BUS/MessageManager.cs
+++ BUS/MessageManager.cs
@@ -21,16 +21,19 @@ namespace BUS
                 ExecuteSingle<Message>();
         }
 
-        public static List<Message> GetListMessage()
+        public static List<Message> GetListMessageByStatus(int id)
         {
-            return new Select().From(Message.Schema.TableName).InnerJoin<User>().ExecuteTypedList<Message>();
+            InlineQuery qry = new InlineQuery();
+            List<Message> MessageList = qry.ExecuteTypedList<Message>($"Select * From Messages inner join users on users.id = messages.userid where messages.status = '{id}'");
+
+            return MessageList;
         }
 
-        public static List<Message> GetListMessageById(int id)
-        {
-            return new Select().From(Message.Schema.TableName).Where(Message.Columns.Status).IsEqualTo(id)
-                .ExecuteTypedList<Message>();
-        }
+        //public static List<Message> GetListMessageById(int id)
+        //{
+        //    return new Select().From(Message.Schema.TableName).Where(Message.Columns.Status).IsEqualTo(id)
+        //        .ExecuteTypedList<Message>();
+        //}
 
 
     }
