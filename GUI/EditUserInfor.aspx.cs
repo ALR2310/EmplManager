3115cccc655fa569038e1025a0d7b2eba16c687d
Gnihc <meeh69420@gmail.com>
2023-07-10 10:57:36 +0700

    ,

--- GUI/EditUserInfor.aspx.cs
+++ GUI/EditUserInfor.aspx.cs
@@ -6,23 +6,36 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Diagnostics;
 using DAL;
+using BUS;
+
 namespace GUI
 {
     public partial class EditUserInfor : System.Web.UI.Page
     {
         private User UserFromCookie;
+
+        private void AssignInfo()
+        {
+            tblEmail.Text = UserFromCookie.Email;
+            tblDisplayName.Text = UserFromCookie.DisplayName;
+            tblUserName.Text = UserFromCookie.UserName;
+        }
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
             {
                 UserFromCookie = MyLayout.UserFromCookie;
+                AssignInfo();
 
             }
         }
 
         protected void btnSave_Click(object sender, EventArgs e)
         {
-            Debug.WriteLine("A");
+            if (UserFromCookie.Id == UserManager.getTokenUser(Request.Cookies["AuthToken"].Value).Id)
+            {
+                ToastManager.SuccessToast("Lưu thay đổi thành công!");
+            }
             return;
         }
     }
