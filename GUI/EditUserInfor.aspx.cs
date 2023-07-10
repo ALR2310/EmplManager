3d1fda9d57afc4459d685f42a97e1dac4af1c0d3
Gnihc <meeh69420@gmail.com>
2023-07-10 11:24:12 +0700

    .

--- GUI/EditUserInfor.aspx.cs
+++ GUI/EditUserInfor.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 using System.Diagnostics;
 using DAL;
 using BUS;
-
+using System.IO;
 namespace GUI
 {
     public partial class EditUserInfor : System.Web.UI.Page
@@ -19,6 +19,7 @@ namespace GUI
             tblEmail.Text = UserFromCookie.Email;
             tblDisplayName.Text = UserFromCookie.DisplayName;
             tblUserName.Text = UserFromCookie.UserName;
+            ImageAvatar.ImageUrl = UserFromCookie.Avatar;
         }
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -35,15 +36,36 @@ namespace GUI
             if (IsPostBack)
             {
                 User CheckingValidUser = UserManager.getTokenUser(Request.Cookies["AuthToken"].Value);
-
+                if (CheckingValidUser == null) { return; }
 
                 Debug.WriteLine(UserFromCookie.Id);
                 Debug.WriteLine(CheckingValidUser.Id);
                 if (UserFromCookie.Id == CheckingValidUser.Id)
                 {
+                    if (uploadAvatar.HasFile)
+                    {
+                        // Get the file name and extension
+                        string fileName = $"U_{CheckingValidUser.Id}";
+                        string extension = Path.GetExtension(fileName);
+
+                        // Specify the directory to save the file
+                        string uploadDirectory = Server.MapPath("~/Images/Avatar/Uploads");
+
+                        // Generate a unique file name
+                        string uniqueFileName = fileName + extension;
+
+                        // Save the file to the server
+                        uploadAvatar.SaveAs(Path.Combine(uploadDirectory, uniqueFileName));
+                        UserFromCookie.Avatar = Path.Combine(uploadDirectory, uniqueFileName);
+                        // Display a success message or perform further processing
+                        // ...
+                    }
+
                     UserFromCookie.Email = tblEmail.Text;
                     UserFromCookie.DisplayName = tblDisplayName.Text;
                     UserFromCookie.UserName = tblUserName.Text;
+
+          
                     UserFromCookie.Save(); 
                     Debug.WriteLine("Lưu thàng công");
                     
