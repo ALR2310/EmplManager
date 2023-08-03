a455308d7d8a8f1f77c7984e8824a1ef93a8eaf3
Gnihc <meeh69420@gmail.com>
2023-08-03 08:55:32 +0700

    .

--- GUI/JS/image_attach.js
+++ GUI/JS/image_attach.js
@@ -1,3 +1,10 @@
-﻿function openAttachMenu(event) {
+﻿let uploadInput = $("#uploadInput");
+function openAttachMenu(event) {
     event.preventDefault();
+    uploadInput.click();
 }
+
+uploadInput.on('change', function (e) {
+    console.log(uploadInput);
+    console.log(uploadInput[0].files);
+});
